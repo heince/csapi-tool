5e8b921314d740117ce2555dac53900f6a614295
Heince <heince@gmail.com>
2012-02-16 15:14:23 +0800

    add script to generate encoded ldapConfig API url

---
+++ bin/genldapconfigurl.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+
+#this script generate url for ldapConfig API
+#set your parameter below
+
+use URI::Escape;
+ 
+my ($apiurl, $command, $ldapsvr, $searchbase, $queryfilter, $binddn, $bindpass, $encode); 
+
+$apiurl = q|http://192.168.1.109:8096/client/api?/|;
+$command = q|command=ldapConfig|;
+$ldapsvr = q|myldap.example.com|;
+$searchbase = q|cn=users,dc=example,dc=com|;
+$queryfilter = q|(&(uid=%u))|;
+$binddn = q|cn=Administrator,cn=users,dc=example,dc=com|;
+$bindpass = q|test123|;
+
+$encode = "$apiurl$command&hostname=$ldapsvr&searchbase=" . uri_escape($searchbase) . "&queryfilter=" . uri_escape($queryfilter) .
+            "&binddn=" . uri_escape($binddn) . "&bindpass=$bindpass&response=json";
+
+print "$encode\n";
