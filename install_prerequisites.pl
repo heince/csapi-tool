63d05e9e248a9023bf196f47d7f76d5070ca6046
Heince Kurniawan <heince@gmail.com>
2012-05-29 21:57:04 +0800

    add CLI::Framework

--- install_prerequisites.pl
+++ install_prerequisites.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/env perl
 
 my $requires = 'Mouse DBI DBD::mysql WWW::Mechanize URI::Encode XML::Simple XML::Twig XML::LibXML
-                JSON LWP::Protocol::https  Digest::SHA';
+                JSON LWP::Protocol::https  Digest::SHA CLI::Framework';
 system("cpan $requires");
