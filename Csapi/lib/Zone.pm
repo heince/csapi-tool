33a0b468afc716a5312009f89829d66fe0cfee78
Heince Kurniawan <heince@gmail.com>
2012-05-30 20:22:39 +0800

    new lib for Zone

---
+++ Csapi/lib/Zone.pm
@@ -0,0 +1,34 @@
+package Zone;
+use v5.10;
+use Mouse;
+use strict;
+use warnings;
+use GenUrl;
+
+extends 'GenUrl';
+
+sub set_list_xml{
+    my $self = shift;
+    
+    $self->xmltmp(XML::LibXML->load_xml(location => "$ENV{'CSAPI_ROOT'}/config/Zone/list.xml"));
+}
+
+sub list_zones{
+    my $self = shift;
+    
+    #set xmltmp
+    $self->set_list_xml();
+    
+    #set initial command and xmlresult attr
+    $self->set_command();
+   
+    #set id if not set
+    if(defined $self->uuid){        
+        $self->command($self->command . "&id=" . $self->uuid) unless $self->param =~ /\bid=\b/;   
+    }
+   
+    #print the output
+    $self->get_output();
+}
+
+1;
