a900b809139a911738c589185a15ced113cf6991
Heince Kurniawan <heince@gmail.com>
2012-06-15 18:38:11 +0800

    add support to list domains

--- Csapi/lib/Field.pm
+++ Csapi/lib/Field.pm
@@ -13,12 +13,13 @@ has [qw /haenabled cpuspeed nic-type hosttags issystem memory tags bootable form
 has [qw /cpunumber vmrunning vmstopped isdefault/] => ( is => 'rw', isa => 'Int', default => 11 );
 
 has [qw /ipavailable hypervisor memory state user-state defaultuse disksize crossZones isfeatured type usage
-     rawusage usagetype/] => ( is => 'rw', isa => 'Int', default => 12 );
+     rawusage usagetype acltype/] => ( is => 'rw', isa => 'Int', default => 12 );
 
 has [qw /vmavailable volumelimit volumetotal user-domain limitcpuuse networkrate storagetype templatetag
      ostypename jobstatus networktype/] => ( is => 'rw', isa => 'Int', default => 13 );
 
-has [qw /templatelimit templatetotal user-account systemvmtype iscustomized templatetype/] => ( is => 'rw', isa => 'Int', default => 14 );
+has [qw /templatelimit templatetotal user-account systemvmtype iscustomized templatetype
+     broadcasturi/] => ( is => 'rw', isa => 'Int', default => 14 );
 
 has [qw /account domain group isoname password nic-ip nic-isdefault securitygroup-domain jobprocstatus jobresultcode jobresulttype
      securitygroup-name networkdomain receivedbytes sentbytes snapshottotal username isextractable domainname
@@ -27,8 +28,9 @@ has [qw /account domain group isoname password nic-ip nic-isdefault securitygrou
 has [qw /rootdevicetype securitygroup-account accounttype user-firstname user-lastname
      firstname lastname/] => ( is => 'rw', isa => 'Int', default => 16 );
 
-has [qw /ipaddress networkkbsread passwordenabled nic-traffictype volumeavailable allocationstate dhcpprovider
-     passwordenabled dns1 dns2 internaldns1 internaldns2 /] => ( is => 'rw', isa => 'Int', default => 17 );
+has [qw /ipaddress netmask gateway networkkbsread passwordenabled nic-traffictype volumeavailable allocationstate dhcpprovider
+     passwordenabled dns1 dns2 internaldns1 internaldns2 restartrequired specifyipranges subdomainaccess traffictype
+     /] => ( is => 'rw', isa => 'Int', default => 17 );
 
 has [qw /networkkbswrite nic-broadcasturi nic-isolationuri nic-macaddress nic-netmask
      securitygroup-jobstatus user-accounttype/] => ( is => 'rw', isa => 'Int', default => 18 );
@@ -36,8 +38,8 @@ has [qw /networkkbswrite nic-broadcasturi nic-isolationuri nic-macaddress nic-ne
 has [qw /forvirtualnetwork iscleanuprequired snapshotavailable snapshotlimit templateavailable jobinstancetype
      /] => ( is => 'rw', isa => 'Int', default => 19 );
 
-has [qw /hostname isodisplaytext user-timezone displaytext description timezone guestcidraddress
-     /] => ( is => 'rw', isa => 'Int', default => 20 );
+has [qw /hostname isodisplaytext user-timezone displaytext description timezone guestcidraddress broadcastdomaintype
+     cidr/] => ( is => 'rw', isa => 'Int', default => 20 );
 
 has [qw /serviceofferingname templatedisplaytext securitygroup-ingressrule-cidr/] => ( is => 'rw', isa => 'Int', default => 21 );
 
@@ -45,21 +47,23 @@ has [qw /securitygroup-ingressrule-account securitygroupsenabled/] => ( is => 'r
 
 has [qw /securitygroup-ingressrule-endport/] => ( is => 'rw', isa => 'Int', default => 24 );
 
-has [qw /name displayname created securitygroup-description securitygroup-ingressrule-icmpcode securitygroup-ingressrule-icmptype
-     securitygroup-ingressrule-protocols user-created user-email checksum removed zonename email
+has [qw /displayname created securitygroup-description securitygroup-ingressrule-icmpcode securitygroup-ingressrule-icmptype
+     securitygroup-ingressrule-protocols user-created user-email checksum removed zonename email project service
      /] => ( is => 'rw', isa => 'Int', default => 25 );
 
-has [qw /enddate startdate/] => ( is => 'rw', isa => 'Int', default => 30 );
+has [qw /enddate startdate networkofferingavailability/] => ( is => 'rw', isa => 'Int', default => 30 );
 
-has [qw /securitygroup-ingressrule-securitygroupname vlan/] => ( is => 'rw', isa => 'Int', default => 34 );
+has [qw /securitygroup-ingressrule-securitygroupname vlan name/] => ( is => 'rw', isa => 'Int', default => 34 );
 
 has [qw /securitygroup-ingressrule-startport templatename/] => ( is => 'rw', isa => 'Int', default => 35 );
 
 has [qw/id accountid domainid groupid guestosid hostid isoid templateid jobid rootdeviceid serviceofferingid sourcetemplateid
      diskofferingid zoneid nic-id nic-gateway nic-networkid securitygroup-id securitygroup-domainid securitygroup-jobid
      securitygroup-ingressrule-ruleid user-id user-domainid ostypeid networkid usageid virtualmachineid userid zonetoken
-     offeringid jobinstanceid cmd/] => ( is => 'rw', isa => 'Int', default => 38 );
+     offeringid jobinstanceid cmd networkofferingid networkofferingname physicalnetworkid projectid related
+     /] => ( is => 'rw', isa => 'Int', default => 38 );
 
-has [qw /user-apikey user-secretkey apikey secretkey jobresult/] => ( is => 'rw', isa => 'Int', default => 88 );
+has [qw /user-apikey user-secretkey apikey secretkey jobresult networkofferingdisplaytext
+     /] => ( is => 'rw', isa => 'Int', default => 88 );
 
 1;
