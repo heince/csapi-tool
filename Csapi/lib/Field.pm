73e5603589a983a61002a7bef6b6d02512ea3d36
Heince Kurniawan <heince@gmail.com>
2012-07-19 00:47:09 +0800

    add more attributes for firewall field

--- Csapi/lib/Field.pm
+++ Csapi/lib/Field.pm
@@ -4,18 +4,18 @@ use Mouse;
 use strict;
 use warnings;
 
-has [qw /cpuused iplimit iptotal vmlimit vmtotal offerha isready status level haschild port
+has [qw /cpuused iplimit iptotal vmlimit vmtotal offerha isready status level haschild port endport icmpcode icmptype
      /] => ( is => 'rw', isa => 'Int', default => 9 );
 
 has [qw /haenabled cpuspeed nic-type hosttags issystem memory tags bootable format ispublic success
-     size/] => ( is => 'rw', isa => 'Int', default => 10 );
+     size protocol/] => ( is => 'rw', isa => 'Int', default => 10 );
 
-has [qw /cpunumber vmrunning vmstopped isdefault/] => ( is => 'rw', isa => 'Int', default => 11 );
+has [qw /cpunumber vmrunning vmstopped isdefault startport/] => ( is => 'rw', isa => 'Int', default => 11 );
 
 has [qw /ipavailable hypervisor memory state user-state defaultuse disksize crossZones isfeatured type usage
-     rawusage usagetype acltype/] => ( is => 'rw', isa => 'Int', default => 12 );
+     rawusage usagetype acltype publicport/] => ( is => 'rw', isa => 'Int', default => 12 );
 
-has [qw /vmavailable volumelimit volumetotal user-domain limitcpuuse networkrate storagetype templatetag
+has [qw /vmavailable volumelimit volumetotal user-domain limitcpuuse networkrate storagetype templatetag privateport
      ostypename jobstatus networktype/] => ( is => 'rw', isa => 'Int', default => 13 );
 
 has [qw /templatelimit templatetotal user-account systemvmtype iscustomized templatetype
@@ -23,10 +23,10 @@ has [qw /templatelimit templatetotal user-account systemvmtype iscustomized temp
 
 has [qw /account domain group isoname password nic-ip nic-isdefault securitygroup-domain jobprocstatus jobresultcode jobresulttype
      securitygroup-name networkdomain receivedbytes sentbytes snapshottotal username isextractable domainname path parentdomainname
-     scriptsversion/] => ( is => 'rw', isa => 'Int', default => 15 );
+     scriptsversion publicendport/] => ( is => 'rw', isa => 'Int', default => 15 );
 
 has [qw /rootdevicetype securitygroup-account accounttype user-firstname user-lastname redundantstate
-     firstname lastname/] => ( is => 'rw', isa => 'Int', default => 16 );
+     firstname lastname privateendport/] => ( is => 'rw', isa => 'Int', default => 16 );
 
 has [qw /ipaddress netmask gateway networkkbsread passwordenabled nic-traffictype volumeavailable allocationstate dhcpprovider
      passwordenabled dns1 dns2 internaldns1 internaldns2 restartrequired specifyipranges subdomainaccess traffictype
@@ -40,7 +40,7 @@ has [qw /forvirtualnetwork iscleanuprequired snapshotavailable snapshotlimit tem
      isredundantrouter/] => ( is => 'rw', isa => 'Int', default => 19 );
 
 has [qw /hostname isodisplaytext user-timezone displaytext description timezone guestcidraddress broadcastdomaintype
-     cidr name/] => ( is => 'rw', isa => 'Int', default => 20 );
+     cidr name cidrlist/] => ( is => 'rw', isa => 'Int', default => 20 );
 
 has [qw /serviceofferingname templatedisplaytext securitygroup-ingressrule-cidr/] => ( is => 'rw', isa => 'Int', default => 21 );
 
@@ -50,9 +50,9 @@ has [qw /securitygroup-ingressrule-endport/] => ( is => 'rw', isa => 'Int', defa
 
 has [qw /displayname created securitygroup-description securitygroup-ingressrule-icmpcode securitygroup-ingressrule-icmptype
      securitygroup-ingressrule-protocols user-created user-email checksum removed zonename email project service
-     /] => ( is => 'rw', isa => 'Int', default => 25 );
+     virtualmachinename/] => ( is => 'rw', isa => 'Int', default => 25 );
 
-has [qw /enddate startdate networkofferingavailability/] => ( is => 'rw', isa => 'Int', default => 30 );
+has [qw /enddate startdate networkofferingavailability virtualmachinedisplayname/] => ( is => 'rw', isa => 'Int', default => 30 );
 
 has [qw /securitygroup-ingressrule-securitygroupname vlan/] => ( is => 'rw', isa => 'Int', default => 34 );
 
@@ -62,7 +62,7 @@ has [qw/id accountid domainid groupid guestosid hostid isoid templateid jobid ro
      diskofferingid zoneid nic-id nic-gateway nic-networkid securitygroup-id securitygroup-domainid securitygroup-jobid
      securitygroup-ingressrule-ruleid user-id user-domainid ostypeid networkid usageid virtualmachineid userid zonetoken
      offeringid jobinstanceid cmd networkofferingid networkofferingname physicalnetworkid projectid related parentdomainid
-     queryfilter guestnetworkid linklocalnetworkid podid publicnetworkid/] => ( is => 'rw', isa => 'Int', default => 38 );
+     queryfilter guestnetworkid linklocalnetworkid podid publicnetworkid ipaddressid/] => ( is => 'rw', isa => 'Int', default => 38 );
 
 has [qw /searchbase/] => ( is => 'rw', isa => 'Int', default => 50 );
 
